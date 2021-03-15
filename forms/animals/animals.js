34de27c307eec58c730edfc1eb32f4fdbea7567d
Ethan Hall <ethanhall99@gmail.com>
2021-03-15 15:41:06 -0500

    Initial commit

---
+++ forms/animals/animals.js
@@ -0,0 +1,8 @@
+// animals
+
+let myAnimals = ["dog", "cat", "horse", "meerkat"]
+
+let newAnimal = promt("Enter an animal")
+myAnimals.push(newAnimal.toLowerCase())
+
+console.log(`The last animal is a/an ${myAnimals[-1]}`)
